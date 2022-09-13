0a71984065615a2ac19651f1888871362993f1ab
Sergio_Cardoso <sergio.cardoso@people-media.com.mx>
2022-09-13 18:33:46 -0500

    cambios en encriptacion

--- src/app/Services/encriptar.service.ts
+++ src/app/Services/encriptar.service.ts
@@ -6,52 +6,52 @@ import * as CryptoJS from 'crypto-js';
 })
 export class EncriptarService {
 
-  private readonly SALT = CryptoJS.enc.Base64.parse('@1B2c3D4e5F6¿?H8');
-  private readonly ITERATIONS = 1000; 
-
   constructor() { }
   
-  Encrypt(origin: string, key: string): string{
-    try {
-      let keyWordArray = CryptoJS.enc.Utf8.parse(key);
-      let originWordArray = CryptoJS.enc.Utf16LE.parse(origin);
-      let keyAndIv = CryptoJS.PBKDF2(keyWordArray, this.SALT, { 
-        keySize: 256/8, 
-        iterations: this.ITERATIONS, 
-        hasher: CryptoJS.algo.SHA1 
-      });
-      let hexKeyAndIv = CryptoJS.enc.Hex.stringify(keyAndIv);
-      let hexKey = CryptoJS.enc.Hex.parse(hexKeyAndIv.substring(0, 64));
-      let iv = CryptoJS.enc.Hex.parse(hexKeyAndIv.substring(64, hexKeyAndIv.length));
-      return CryptoJS.AES.encrypt(originWordArray, hexKey, {iv: iv}).toString();
-    } 
-    catch (error) {
-      throw error;
-    }
-  }
+private readonly SALT = CryptoJS.enc.Base64.parse("SXZhbiBNZWR2ZWRldg==");
+private readonly ITERATIONS = 1000; 
 
-  Decrypt(encrypted: string, key: string): string{    
-    try {
-      let keyWordArray = CryptoJS.enc.Utf8.parse(key);
-  
-      let keyAndIv = CryptoJS.PBKDF2(keyWordArray, this.SALT, { 
-        keySize: 256/8, 
-        iterations: this.ITERATIONS,
-        hasher: CryptoJS.algo.SHA1 
-      });
-    
-      let hexKeyAndIv = CryptoJS.enc.Hex.stringify(keyAndIv);
-  
-      let hexKey = CryptoJS.enc.Hex.parse(hexKeyAndIv.substring(0, 64));
-      let iv = CryptoJS.enc.Hex.parse(hexKeyAndIv.substring(64, hexKeyAndIv.length)); 
-                                                     
-      let decryptedWordArray = CryptoJS.AES.decrypt(encrypted, hexKey, {iv: iv});
-      return CryptoJS.enc.Utf16LE.stringify(decryptedWordArray);   
-    } 
-    catch (error) {
-      throw error;
-    }
+Encrypt(origin: string, key: string): string{
+  try {
+    let keyWordArray = CryptoJS.enc.Utf8.parse(key);
+    let originWordArray = CryptoJS.enc.Utf16LE.parse(origin);
+    let keyAndIv = CryptoJS.PBKDF2(keyWordArray, this.SALT, { 
+      keySize: 256/32 + 128/32, 
+      iterations: this.ITERATIONS, 
+      hasher: CryptoJS.algo.SHA1 
+    });
+    let hexKeyAndIv = CryptoJS.enc.Hex.stringify(keyAndIv);
+    let hexKey = CryptoJS.enc.Hex.parse(hexKeyAndIv.substring(0, 64));
+    let iv = CryptoJS.enc.Hex.parse(hexKeyAndIv.substring(64, hexKeyAndIv.length));
+    return CryptoJS.AES.encrypt(originWordArray, hexKey, {iv: iv}).toString();
+  } 
+  catch (error) {
+    throw error;
   }
+}
+
+Decrypt(encrypted: string, key: string): string{    
+  try {
+    let keyWordArray = CryptoJS.enc.Utf8.parse(key);
+
+    let keyAndIv = CryptoJS.PBKDF2(keyWordArray, this.SALT, { 
+      keySize: 256/32 + 128/32, 
+      iterations: this.ITERATIONS,
+      hasher: CryptoJS.algo.SHA1 
+    });
+  
+    let hexKeyAndIv = CryptoJS.enc.Hex.stringify(keyAndIv);
+
+    let hexKey = CryptoJS.enc.Hex.parse(hexKeyAndIv.substring(0, 64));
+    let iv = CryptoJS.enc.Hex.parse(hexKeyAndIv.substring(64, hexKeyAndIv.length)); 
+                                                   
+    let decryptedWordArray = CryptoJS.AES.decrypt(encrypted, hexKey, {iv: iv});
+    return CryptoJS.enc.Utf16LE.stringify(decryptedWordArray);   
+  } 
+  catch (error) {
+    throw error;
+  }
+}
 
   // encriptar(plainText: any): string {   
     
